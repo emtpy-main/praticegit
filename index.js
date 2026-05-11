73f8bcc91fa25e1ede95c9fdef5caf0d16dab487
3pratik2 <singhpratik2210@gmail.com>
2026-05-11 19:40:26 +0530

    fixes 222

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log("Hello, World!"); // prev code 
 console.log("Hello");
 console.log("Welcome to the project!"); // new change made by pratik singhgit 
+
+
+
+console.log("Bye messages from fix 222")
